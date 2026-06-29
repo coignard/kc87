bf9656e3208982d87d22ae354dc56b1ba854db90
René Coignard <contact@renecoignard.com>
2026-06-29 16:20:44 +0200

    feat(disk): register disk peripheral module

--- src/core/peripherals/mod.rs
+++ src/core/peripherals/mod.rs
@@ -15,8 +15,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+pub mod disk;
 pub mod keyboard;
 pub mod midi;
+pub mod tape;
 
 use serde::Serialize;
 
